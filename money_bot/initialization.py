f74d2b0c106ca0fb9f98a7db07f722e14196d94e
Ramil Suleymanov <lllumineux@outlook.com>
2019-08-19 11:45:33 +0300

    Add main bot menu

--- money_bot/initialization.py
+++ money_bot/initialization.py
@@ -1,6 +1,7 @@
 import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from pymongo import MongoClient
 from umongo import Instance
 
@@ -12,6 +13,6 @@ except ImportError:
 logging.basicConfig(level=logging.DEBUG)
 
 bot = Bot(config.BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 db = MongoClient(config.DB_HOST)[config.DB_NAME]
 instance = Instance(db)
--- money_bot/utils/markups.py
+++ money_bot/utils/markups.py
@@ -1,1 +1,18 @@
-# This is the bot for any markups
+from aiogram import types
+
+from money_bot.utils.strings import MAIN_MENU_BUTTONS_LABELS
+
+
+def get_main_menu_markup():
+    btns_text = [
+        MAIN_MENU_BUTTONS_LABELS["earn"],
+        MAIN_MENU_BUTTONS_LABELS["play"],
+        MAIN_MENU_BUTTONS_LABELS["balance"],
+        MAIN_MENU_BUTTONS_LABELS["invite"],
+        MAIN_MENU_BUTTONS_LABELS["withdrawal"],
+        MAIN_MENU_BUTTONS_LABELS["rules"],
+    ]
+
+    keyboard_markup = types.ReplyKeyboardMarkup(row_width=2)
+    keyboard_markup.add(*[types.KeyboardButton(btn_text) for btn_text in btns_text])
+    return keyboard_markup
