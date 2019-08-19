02b94ee47bbb336244a74e4c280c3b98f238da56
Danila <d.kuzm03@mail.ru>
2019-08-19 14:59:25 +0300

    fixed version

--- money_bot/utils/states.py
+++ money_bot/utils/states.py
@@ -1,1 +1,7 @@
 # This is a file for any states for FSM
+from aiogram.dispatcher.filters.state import State, StatesGroup
+class states_group(StatesGroup):
+    new_task_ok = State()
+    new_task = State()
+    main_menu = State()
+    button_result = State()
